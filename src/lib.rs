993151d6c3d733b0244fab0b650929671378f8db
bjtha <142571425+bjtha@users.noreply.github.com>
2025-05-10 17:15:42 +0100

    Added: validation of DNA sequence

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub struct Sequence {
     pub name: String,
     pub bases: String,
@@ -22,8 +22,11 @@ impl Sequence {
     }
 }
 
+fn validate_dna(bases: &str) -> bool {
+    bases.chars().all(|c| matches!(c, 'a' | 'c' | 'g' | 't'))
+}
 
-fn extract_sequences(fasta_string: String) -> Vec<Sequence> {
+fn extract_sequences(fasta_string: String) -> Result<Vec<Sequence>, String> {
 
     let lines: Vec<&str> = fasta_string.lines().collect();
 
@@ -40,13 +43,58 @@ fn extract_sequences(fasta_string: String) -> Vec<Sequence> {
             );
             seq_lines.clear()
         } else {
-            seq_lines.insert(0, line.trim().to_lowercase());
+            let clean_line = line.trim().to_lowercase();
+            if validate_dna(&clean_line) {
+                seq_lines.insert(0, clean_line)
+            } else {
+                return Err(String::from("File contains invalid character"))
+            }
         }
     }
-    sequences
+    Ok(sequences)
+}
+
+pub fn reads(fasta_string: String) -> Vec<Sequence> {
+    match extract_sequences(fasta_string) {
+        Ok(seqs) => seqs,
+        Err(msg) => panic!("{}", msg)
+    }
 }
 
 pub fn read(path: &str) -> Vec<Sequence> {
     let contents = fs::read_to_string(path).unwrap();
-    extract_sequences(contents)
+    reads(contents)
 }
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_string() {
+        let fasta = String::from(">A\naattggcc");
+        let sequences = reads(fasta);
+        let first = sequences.first().unwrap();
+
+        assert_eq!(first.bases, "aattggcc");
+    }
+
+    #[test]
+    fn empty_file() {
+        let fasta = String::from("");
+        let sequences = reads(fasta);
+        assert!(sequences.is_empty())
+    }
+
+    #[test]
+    fn bad_character() {
+        let fasta = String::from(">A\nD");
+        assert!(extract_sequences(fasta).is_err())
+    }
+}
+
+// To-Do
+// Add testing
+// Add handling for malformed fasta files.
